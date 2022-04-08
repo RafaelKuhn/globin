51de7b65885430a7589da6bef07f4c951a501fb2
Gabriel Schirmer <gabriel.schirmer@sou.unijui.edu.br>
2022-04-07 21:36:14 -0300

    created vertical states control

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,36 @@
 extends KinematicBody
 
+enum HorizontalStates {DEFAULT, JUMPING, ROLLING}
+#enum vertical_state {}
+var horizontal_state = HorizontalStates.DEFAULT
+
 func _ready():
 	pass # Replace with function body.
+	
+func _process(delta):
+	if Input.is_action_just_pressed("W_key"):
+		try_jumping()
+		
+	if Input.is_action_just_pressed("S_key"):
+		try_rolling()
+			
+func try_jumping():
+	if horizontal_state != HorizontalStates.DEFAULT:
+		return
+	horizontal_state = HorizontalStates.JUMPING
+	print("started jumping")
+	$JumpTimer.start(2.0)
+	
+func try_rolling():
+	if horizontal_state != HorizontalStates.DEFAULT:
+		return
+	horizontal_state = HorizontalStates.ROLLING
+	print("started rolling")
+	$JumpTimer.start(1.0)
+
+
+func _on_JumpTimer_timeout():
+	horizontal_state = HorizontalStates.DEFAULT
+	print("time out vertical")
+	
 
