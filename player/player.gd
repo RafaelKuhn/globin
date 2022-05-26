5767a43af52a460f4133ff2904b50d1cd07be8d7
Gabriel Schirmer <gabriel.schirmer@sou.unijui.edu.br>
2022-05-26 20:22:24 -0300

    added 'animations' to distinguish running and jumping

--- player/player.gd
+++ player/player.gd
@@ -1,18 +1,18 @@
 extends Spatial
 
 enum VerticalStates { DEFAULT, JUMPING, ROLLING }
-enum HorizontalState { RUNNING, SWITCHING }
+enum HorizontalStates { RUNNING, SWITCHING }
 
-export var lane_switch_delay := 0.2
+export var lane_switch_delay := 0.3
 export var lane_switch_allowed_gap := 0.7
 
 var vertical_state = VerticalStates.DEFAULT
-var horizontal_state = HorizontalState.RUNNING
+var horizontal_state = HorizontalStates.RUNNING
 
 var lane: int
 var previous_lane: int
 
-var anim_progress := 0.0
+var anim_h_progress := 0.0
 
 
 func _ready():
@@ -33,7 +33,8 @@ func _process(delta: float):
 	if Input.is_action_just_pressed("A_key"):
 		try_switching_left()
 
-	update_animation(delta)
+	update_horizontal_animation(delta)
+	update_vertical_animation(delta)
 
 
 # vertical:
@@ -42,7 +43,7 @@ func try_jumping():
 		return
 	vertical_state = VerticalStates.JUMPING
 	print("jumping")
-	$JumpTimer.start(2.0)
+	$JumpTimer.start(1.3)
 	
 func try_rolling():
 	if vertical_state != VerticalStates.DEFAULT:
@@ -58,23 +59,23 @@ func _on_JumpTimer_timeout():
 
 # horizontal:
 func try_switching_right():
-	if horizontal_state != HorizontalState.RUNNING:
+	if horizontal_state != HorizontalStates.RUNNING:
 		return
 	if lane == 3:
 		return
 	
 	switch_right()
-	horizontal_state = HorizontalState.SWITCHING
+	horizontal_state = HorizontalStates.SWITCHING
 	$LaneTimer.start(lane_switch_delay)
 	
 func try_switching_left():
-	if horizontal_state != HorizontalState.RUNNING:
+	if horizontal_state != HorizontalStates.RUNNING:
 		return
 	if lane == 1:
 		return
 	
 	switch_left()
-	horizontal_state = HorizontalState.SWITCHING
+	horizontal_state = HorizontalStates.SWITCHING
 	$LaneTimer.start(lane_switch_delay)
 
 
@@ -82,28 +83,43 @@ func try_switching_left():
 func switch_right():
 	previous_lane = lane
 	lane = lane + 1
-	anim_progress = 0.0
-	# print("vai trocar, animation progress: %f " % anim_progress)
+	anim_h_progress = 0.0
+	# print("vai trocar, animation progress: %f " % anim_h_progress)
 
 func switch_left():
 	previous_lane = lane
 	lane = lane - 1
-	anim_progress = 0.0
-	# print("vai trocar, animation progress: %f " % anim_progress)
+	anim_h_progress = 0.0
+	# print("vai trocar, animation progress: %f " % anim_h_progress)
 
 
 # fim da troca de lane
 func _on_LaneTimer_timeout():
-	horizontal_state = HorizontalState.RUNNING
-	# print("trocou, animation progress: %f " % anim_progress)
+	horizontal_state = HorizontalStates.RUNNING
+	# print("trocou, animation progress: %f " % anim_h_progress)
 
 
-func update_animation(delta: float):
-	if horizontal_state != HorizontalState.SWITCHING:
+func update_horizontal_animation(delta: float):
+	if horizontal_state != HorizontalStates.SWITCHING:
 		return
 
-	anim_progress += delta * 1/lane_switch_delay
-	translation.x = lerp(previous_lane, lane, anim_progress)
+	anim_h_progress += delta * 1/lane_switch_delay
+	translation.x = lerp(previous_lane, lane, anim_h_progress)
+
+func update_vertical_animation(_delta: float):
+	match vertical_state:
+		VerticalStates.JUMPING:
+			$Sprite/Animated.animation = "jumping"
+			translation.y = 2
+		VerticalStates.ROLLING:
+			scale.y = 0.7
+			scale.x = 1.5		
+		VerticalStates.DEFAULT:
+			$Sprite/Animated.animation = "running"
+			translation.y = 0
+			scale.y = 1
+			scale.x = 1
+			
 
 
 var Globals = preload("res://globals.gd")
@@ -111,7 +127,7 @@ var Globals = preload("res://globals.gd")
 # eventos
 func _on_any_obstacle_z_collision(lane_obstacle_x: int, obj_type):
 	var is_collision_in_the_same_lane = lane_obstacle_x == lane
-	var is_coliision_while_switching_lanes = lane_obstacle_x == previous_lane && anim_progress < lane_switch_allowed_gap
+	var is_coliision_while_switching_lanes = lane_obstacle_x == previous_lane && anim_h_progress < lane_switch_allowed_gap
 	if is_collision_in_the_same_lane || is_coliision_while_switching_lanes:
 		try_to_collide_based_on_type(obj_type)
 		
@@ -120,6 +136,7 @@ func _on_any_obstacle_z_collision(lane_obstacle_x: int, obj_type):
 func try_to_collide_based_on_type(obj_type):
 	match obj_type:
 		Globals.OBSTACLE_TYPE.PREDA:
+			
 			print("preda pegou você, se fudeu")
 		Globals.OBSTACLE_TYPE.GIFA:
 			print("girafa pegou você, ta abaixado meo?")
